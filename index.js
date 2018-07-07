3b1a9310588bf663403e2e49241696d52edb4b02
jenniferlong8 <jlong8@bu.edu>
2018-07-07 06:26:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+//  return document.querySelector('div#nested div div div div')
+//  return document.querySelector('#nested').querySelector('.target')
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i=0; i<lis.length; i++) {
+    lis[i].innerHTML = ((parseInt(lis[i].innerHTML)+n))
+  }
+}
+
+function deepestChild() {
+  var deepestChild = document.getElementById('grand-node').querySelectorAll('div')
+  return deepestChild[deepestChild.length-1]
+}
